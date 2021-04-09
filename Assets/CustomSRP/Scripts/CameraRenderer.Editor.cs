1267661a8a6e1102f102ab12e84feebbc289396e
Luft <15201225365@163.com>
2021-04-09 16:59:53 +0800

    upload_chapter1

---
+++ Assets/CustomSRP/Scripts/CameraRenderer.Editor.cs
@@ -0,0 +1,59 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+partial class CameraRenderer
+{
+#if UNITY_EDITOR
+    string SampleName { get; set; }
+#else
+    const string SampleName = bufferName;
+#endif
+
+#if UNITY_EDITOR
+    static ShaderTagId[] legacyShaderTagIds =
+    {
+        new ShaderTagId("Always"),
+        new ShaderTagId("ForwardBase"),
+        new ShaderTagId("PrepassBase"),
+        new ShaderTagId("Vertex"),
+        new ShaderTagId("VertexLMRGBM"),
+        new ShaderTagId("VertexLM")
+    };
+    static Material errorMaterial;
+
+    void DrawGizmos()
+    {
+        if(Handles.ShouldRenderGizmos())
+        {
+            context.DrawGizmos(camera, GizmoSubset.PreImageEffects);
+            context.DrawGizmos(camera, GizmoSubset.PostImageEffects);
+        }
+    }
+
+    void DrawUnsupportedShaders()
+    {
+        if (errorMaterial == null)
+            errorMaterial = new Material(Shader.Find("Hidden/InternalErrorShader"));
+        var drawingSettings = new DrawingSettings(legacyShaderTagIds[0], new SortingSettings(camera));
+        drawingSettings.overrideMaterial = errorMaterial;
+        for (int i = 1; i < legacyShaderTagIds.Length; i++)
+        {
+            drawingSettings.SetShaderPassName(i, legacyShaderTagIds[i]);
+        }
+        var filteringSettings = FilteringSettings.defaultValue;
+        context.DrawRenderers(cullingResoults, ref drawingSettings, ref filteringSettings);
+    }
+
+    void PrepareForSceneWindow()
+    {
+        if (camera.cameraType == CameraType.SceneView)
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+    }
+
+    void PrepareBuffer()
+    {
+        buffer.name = SampleName = camera.name;
+    }
+#endif
+}
---
+++ Assets/CustomSRP/Scripts/CameraRenderer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+partial class CameraRenderer
+{
+    const string bufferName = "Render Camera";
+    CommandBuffer buffer = new CommandBuffer() { name = bufferName };
+    CullingResults cullingResoults;
+
+    ScriptableRenderContext context;
+    Camera camera;
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+        PrepareBuffer();
+
+#if UNITY_EDITOR
+
+        PrepareForSceneWindow();
+#endif
+
+        if (!Cull())
+            return;
+
+        SetUp();
+        DrawVisiableGeometry();
+#if UNITY_EDITOR
+        DrawUnsupportedShaders();
+        DrawGizmos();
+#endif
+        Submit();
+    }
+
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+    private void DrawVisiableGeometry()
+    {
+        //Draw Opaque
+        var sortingSettings = new SortingSettings(camera);
+        sortingSettings.criteria = SortingCriteria.CommonOpaque;
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+        context.DrawRenderers(cullingResoults, ref drawingSettings, ref filteringSettings);
+        //Draw Skybox
+        context.DrawSkybox(camera);
+        //Draw Transparent
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(cullingResoults, ref drawingSettings, ref filteringSettings);
+    }
+
+    bool Cull()
+    { 
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResoults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+
+    private void SetUp()
+    {
+        context.SetupCameraProperties(camera);
+        CameraClearFlags flags = camera.clearFlags;
+        buffer.ClearRenderTarget(
+            flags <= CameraClearFlags.Depth,
+            flags == CameraClearFlags.Color,
+            flags == CameraClearFlags.Color ? camera.backgroundColor.linear : Color.clear);
+        buffer.BeginSample(SampleName);
+        ExecuteBuffer();
+    }
+
+    private void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    private void Submit()
+    {
+        buffer.EndSample(SampleName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+
+}
